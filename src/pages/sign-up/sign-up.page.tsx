68ad4cc7a64c91dff48894c4679a4d24bcf3fd96
RiczCalixto <rcalixtos@gmail.com>
2020-07-25 02:20:16 -0300

    Add sign up page

--- src/pages/sign-up/sign-up.page.tsx
+++ src/pages/sign-up/sign-up.page.tsx
@@ -5,17 +5,21 @@ import {
   Background,
 } from '../../component/styled-components';
 import appLogo from '../../assets/logo.svg';
-import { FiLogIn, FiLock, FiMail } from 'react-icons/fi';
+import { FiArrowLeft, FiLock, FiMail, FiUser } from 'react-icons/fi';
 import { Button } from '../../component/Button.component';
 import { Input } from '../../component/Input.component';
+import { Form } from '@unform/web';
 
-export const SignupPage: React.FC = () => {
+export const SignUpPage: React.FC = () => {
+  const handleSubmit = (data: object): void => console.log(data);
   return (
     <Container>
+      <Background isSignUpPage={true} />
       <Content>
         <img src={appLogo} alt="Barber App" />
-        <form>
-          <h1>Faça seu Login</h1>
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input name="name" placeholder="Nome" type="text" icon={FiUser} />
           <Input name="email" placeholder="E-mail" type="email" icon={FiMail} />
           <Input
             name="password"
@@ -23,15 +27,13 @@ export const SignupPage: React.FC = () => {
             type="password"
             icon={FiLock}
           />
-          <Button>Entrar</Button>
-          <a href="forgot">Esqueci minha senha</a>
-        </form>
+          <Button type="submit">Cadastrar</Button>
+        </Form>
         <a href="http://">
-          <FiLogIn />
-          Criar conta
+          <FiArrowLeft />
+          Voltar para o login
         </a>
       </Content>
-      <Background />
     </Container>
   );
 };

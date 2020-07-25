0539a64693ca3df9b3bc82403d0e84264750b53a
RiczCalixto <rcalixtos@gmail.com>
2020-07-24 22:27:32 -0300

    Add Button and Input component into signup page

--- src/pages/signup/signup.page..tsx
+++ src/pages/signup/signup.page..tsx
@@ -3,9 +3,11 @@ import {
   Content,
   Container,
   Background,
-} from '../../styled-components/components';
+} from '../../component/styled-components';
 import appLogo from '../../assets/logo.svg';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiLock, FiMail } from 'react-icons/fi';
+import { Button } from '../../component/Button.component';
+import { Input } from '../../component/Input.component';
 
 export const SignupPage: React.FC = () => {
   return (
@@ -14,9 +16,14 @@ export const SignupPage: React.FC = () => {
         <img src={appLogo} alt="Barber App" />
         <form>
           <h1>Faça seu Login</h1>
-          <input type="email" placeholder="E-mail" />
-          <input type="password" placeholder="Senha" />
-          <button type="submit">Entrar</button>
+          <Input name="email" placeholder="E-mail" type="email" icon={FiMail} />
+          <Input
+            name="password"
+            placeholder="Senha"
+            type="password"
+            icon={FiLock}
+          />
+          <Button>Entrar</Button>
           <a href="forgot">Esqueci minha senha</a>
         </form>
         <a href="http://">

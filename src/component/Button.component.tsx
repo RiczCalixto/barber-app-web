c4344e45e060edbb03171dbf692f9616559ba856
RiczCalixto <rcalixtos@gmail.com>
2020-07-28 19:57:23 -0300

    Create Modules and Model directories

--- src/component/Button.component.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ButtonHTMLAttributes } from 'react';
-import { StyledButton } from './styled-components';
-
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
-
-export const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
-  return <StyledButton {...rest}>{children}</StyledButton>;
-};

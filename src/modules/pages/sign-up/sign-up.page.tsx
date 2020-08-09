d738f4fbe8484acafe2549219f48c5570b3a6c79
RiczCalixto <rcalixtos@gmail.com>
2020-08-09 16:29:17 -0300

    Create new user

--- src/modules/pages/sign-up/sign-up.page.tsx
+++ src/modules/pages/sign-up/sign-up.page.tsx
@@ -2,7 +2,8 @@ import React, { useCallback, useRef } from 'react';
 import { Form } from '@unform/web';
 import { FiArrowLeft, FiLock, FiMail, FiUser } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import * as Yup from 'yup';
 import {
   Content,
   Container,
@@ -16,17 +17,41 @@ import {
   signUpFormValidation,
   getValidationErrors,
 } from '../../components/validation/form-validation';
+import { api } from '../../../services/api';
+import { useToast } from '../../../hooks/toast-context';
 
 export const SignUpPage: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
-  const handleSubmit = useCallback(async (data: SignUpData): Promise<void> => {
-    try {
-      formRef.current?.setErrors({});
-      await signUpFormValidation(data);
-    } catch (error) {
-      formRef.current?.setErrors(getValidationErrors(error));
-    }
-  }, []);
+  const { addToast } = useToast();
+  const history = useHistory();
+  const handleSubmit = useCallback(
+    async (data: SignUpData): Promise<void> => {
+      try {
+        formRef.current?.setErrors({});
+
+        await signUpFormValidation(data);
+        await api.post('users', data);
+
+        history.push('/');
+
+        addToast({
+          type: 'success',
+          title: 'Cadastro efetuado com sucesso!',
+        });
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          formRef.current?.setErrors(getValidationErrors(error));
+          return;
+        }
+        addToast({
+          type: 'error',
+          title: 'Erro no cadastro',
+          description: 'Ocorreu um erro ao fazer cadastro, tente novamente.',
+        });
+      }
+    },
+    [addToast, history],
+  );
 
   return (
     <Container>
